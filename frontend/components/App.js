b342d4084e2384c31be8a0661f5fb7a916e141a2
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-05-09 14:51:42 -0500

    initial app created

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,18 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todos</h1>
+        <ul>
+          <li>Walk the dog</li>
+          <li>Walk the cat</li>
+          <li>Walk the frog</li>
+        </ul>
+
+        <form>
+          <input /> 
+          <button>Add</button> 
+        </form>
+        <button>Clear</button> 
       </div>
     )
   }
